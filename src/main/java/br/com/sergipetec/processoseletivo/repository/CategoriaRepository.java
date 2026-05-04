ee9ef2956e47007a7aac6779f48cc4cdace5a7ec
Rone Clay <85186341+Ronec1r@users.noreply.github.com>
2026-05-04 13:41:45 -0300

    Delete src directory

--- src/main/java/br/com/sergipetec/processoseletivo/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sergipetec.processoseletivo.repository;
-
-import br.com.sergipetec.processoseletivo.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
