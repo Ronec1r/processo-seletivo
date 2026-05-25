f874a1fdbd576e7e82bf487f1f96af2ff1a298db
Rone Clay <85186341+Ronec1r@users.noreply.github.com>
2026-05-24 21:06:00 -0300

    Add cpfCnpj field to SolicitanteDTO

--- backend/src/main/java/br/com/sergipetec/processoseletivo/dto/SolicitanteDTO.java
+++ backend/src/main/java/br/com/sergipetec/processoseletivo/dto/SolicitanteDTO.java
@@ -4,11 +4,12 @@ package br.com.sergipetec.processoseletivo.dto;
 import br.com.sergipetec.processoseletivo.entity.Solicitante;
 
 public record SolicitanteDTO(Long id,
-                             String nome) {
+                             String nome, String cpfCnpj) {
         public static SolicitanteDTO converterParaDTO(Solicitante solicitante) {
             return new SolicitanteDTO(
                     solicitante.getId(),
-                    solicitante.getNome()
+                    solicitante.getNome(),
+                    solicitante.getCpfCnpj()
             );
         }
 }
